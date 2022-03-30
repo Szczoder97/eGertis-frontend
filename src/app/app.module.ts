ae21c40475d1fb7b4f40a64bdc32b508671d2afc
Pawel <pawelszczo@gmail.com>
2022-03-30 18:08:50 +0200

    comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './components/items/items.component';
+import { ListComponent } from './components/items/list/list.component';
+import { EditComponent } from './components/items/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    ListComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
